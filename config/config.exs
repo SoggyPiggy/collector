06511f7f158ac3cfb84f792ab41384b145fb3381
SoggyPiggy <soggypiggy@pop-os.localdomain>
2020-08-19 07:41:20 -0400

    Deploy to gigalixir

---
+++ config/config.exs
@@ -0,0 +1,31 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
+use Mix.Config
+
+config :collector,
+  ecto_repos: [Collector.Repo]
+
+# Configures the endpoint
+config :collector, CollectorWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "2nlQDb7L1a/8LUZt/NF46/AmcNjcxrWWx/ZZboruOleb95svxVw3z9+p2m1BQjA5",
+  render_errors: [view: CollectorWeb.ErrorView, accepts: ~w(html json)],
+  pubsub: [name: Collector.PubSub, adapter: Phoenix.PubSub.PG2],
+  live_view: [signing_salt: "vvJVaQ9Z"]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"

163f360071ebbf04af64a26bbf510ada9d2d04f0
SoggyPiggy <soggypiggy@pop-os.localdomain>
2020-08-21 23:32:43 -0400

    Selects the command list basedo n the account

--- lib/commands/commands/commands/help.ex
+++ lib/commands/commands/commands/help.ex
@@ -12,11 +12,17 @@ defmodule Commands.Command.Help do
 
   def execute(_args, {account, message}) do
     "__**Help Menu**__\n" <> (
-      Commands.commands(account != nil)
+      account
+      |> get_list()
       |> Enum.map(fn command -> command.module() end)
       |> Enum.map(fn command -> "**#{command.title}**: #{command.description}" end)
       |> Enum.join("\n")
     )
     |> DiscordReceiver.Speaker.send(:direct, message)
   end
+
+  defp get_list(nil), do: Commands.commands_unregistered()
+  defp get_list(account), do: Database.has_admin_override(account) |> get_list(account)
+  defp get_list(true, _), do: Commands.commands_admin()
+  defp get_list(false, _), do: Commands.commands_registered()
 end

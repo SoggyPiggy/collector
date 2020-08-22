12550423aa0b491b972fd2773e4485c41f00e7b6
SoggyPiggy <soggypiggy@pop-os.localdomain>
2020-08-22 00:55:59 -0400

    passes the is_admin data

--- lib/commands/commands/processor.ex
+++ lib/commands/commands/processor.ex
@@ -1,12 +1,15 @@
 defmodule Commands.Processor do
-  def execute(input, {account, _reply_data} = data) do
+  def execute(input, {account, _is_admin, _reply_data} = data) do
     input
     |> process_down_split()
     |> process_command_arguments(account)
     |> execute_command(data)
   end
 
-  defp execute_command({command, arguments}, data), do: command.execute(arguments, data)
+  defp execute_command({command, arguments}, {account, true, reply_data}),
+    do: command.admin_execute(arguments, {account, reply_data})
+  defp execute_command({command, arguments}, {account, false, reply_data}),
+    do: command.execute(arguments, {account, reply_data})
 
   defp process_command_arguments([command | arguments], account) do
     {

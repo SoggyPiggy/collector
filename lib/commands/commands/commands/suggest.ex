d99b0a5e06dc9dad0bd6187cf416fc9a2d4d9b7c
SoggyPiggy <soggypiggy@pop-os.localdomain>
2020-08-28 20:15:12 -0400

    added more information to suggest response

--- lib/commands/commands/commands/suggest.ex
+++ lib/commands/commands/commands/suggest.ex
@@ -2,7 +2,7 @@ defmodule Commands.Command.Suggest do
   @command %Commands.Command{
     id: :suggest,
     title: "Suggest",
-    description: "Make a suggestion for the bot",
+    description: "Make any suggestion for the bot",
     aliases: ["suggest", "suggestion"],
     examples: [">suggest There should be coins for pickles"]
   }
@@ -21,8 +21,31 @@ defmodule Commands.Command.Suggest do
     |> confirm_recording(message)
   end
 
-  defp confirm_recording({:ok, _suggestion}, message) do
-    "Suggestion has been recorded"
+  defp confirm_recording({:ok, suggestion}, message) do
+    """
+    Suggestion has been recorded. `#{format_id(suggestion.id)}`
+    **"**#{format_content(suggestion.content)}**"**
+    """
     |> Discord.send(:reply, message)
   end
+
+  defp format_id(id) do
+    id
+    |> Integer.to_string()
+    |> String.pad_leading(3, "0")
+  end
+
+  defp format_content(content) do
+    content
+    |> String.length()
+    |> format_content(content)
+  end
+  defp format_content(length, content) when length > 255 do
+    (
+      content
+      |> String.slice(0, 251)
+    )
+    <> "..."
+  end
+  defp format_content(_length, content), do: content
 end

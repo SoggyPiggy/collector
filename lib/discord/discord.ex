9788ed9856b81a84f9920be8c7724988f6e20fe6
SoggyPiggy <soggypiggy@pop-os.localdomain>
2020-09-02 23:22:01 -0400

    added delegate to Nostrum create_dm!

--- lib/discord/discord.ex
+++ lib/discord/discord.ex
@@ -3,4 +3,6 @@ defmodule Discord do
 
   defdelegate send(message, destination, data), to: Speaker
   defdelegate send(message, channel_id), to: Speaker
+
+  defdelegate get_dm_channel(discord_id), to: Nostrum.Api, as: :create_dm!
 end

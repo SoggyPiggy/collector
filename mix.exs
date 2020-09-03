db9d44aed5942f2912092436f55c0616430c4525
SoggyPiggy <soggypiggy@pop-os.localdomain>
2020-09-02 23:14:48 -0400

    added httpoison to dependencies

--- mix.exs
+++ mix.exs
@@ -42,6 +42,7 @@ defmodule Collector.MixProject do
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
+      {:httpoison, "~> 1.7"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
       {:nostrum, "~> 0.4"},

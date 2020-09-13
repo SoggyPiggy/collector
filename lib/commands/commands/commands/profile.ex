3fa5ac44964cdcb07a5a8ea92eb88b210a67770a
SoggyPiggy <soggypiggy@pop-os.localdomain>
2020-09-13 05:31:32 -0400

    updated profile

--- lib/commands/commands/commands/profile.ex
+++ lib/commands/commands/commands/profile.ex
@@ -10,20 +10,30 @@ defmodule Commands.Command.Profile do
 
   def module(), do: @command
 
-  def execute("", {%Database.Repo.Account{} = account, message}) do
-    account
-    |> send_account(message)
-  end
-
-  def execute(args, {_account, message}) do
+  def execute(args, {account, _message} = reply_data) do
     args
-    |> String.trim()
-    |> String.replace(~r/<@!?|>/, "")
-    |> String.to_integer()
-    |> Database.Account.get()
-    |> send_account(message)
+    |> OptionParser.split()
+    |> OptionParser.parse(strict: @command.args_strict, aliases: @command.args_aliases)
+    |> check_arguments(account)
+    |> send_reply(reply_data)
   end
 
-  defp send_account(nil, _message), do: nil
-  defp send_account(account, message), do: Discord.send("", account, :reply, message)
+  defp check_arguments({_params, [_ | [_ | _]], _errors}, _account), do: {:error, "Too many users given"}
+  defp check_arguments({_params, [], _errors}, account) do
+    {:ok, account}
+    |> check_arguments_verify()
+  end
+  defp check_arguments({_params, [user], _errors}, _account) do
+    {:ok, Database.Account.get(user)}
+    |> check_arguments_verify()
+  end
+
+  defp check_arguments_verify({:ok, {:ok, item}}), do: {:ok, item}
+  defp check_arguments_verify({:ok, %Database.Repo.Account{}} = data), do: data
+  defp check_arguments_verify({:ok, _}), do: {:error, "User not found"}
+
+  defp send_reply({:error, reason}, {%{discord_id: id}, message}),
+    do: Discord.send("<@#{id}>, #{reason}", :reply, message)
+  defp send_reply({:ok, account}, {_account, message}),
+    do: Discord.send("", account, :reply, message)
 end
